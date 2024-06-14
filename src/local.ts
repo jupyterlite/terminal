ad8649cd7e731b3d300b578b281e0b17703bd65b
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2024-06-14 09:09:06 +0000

    compile worker separately

--- src/local.ts
+++ src/local.ts
@@ -1,5 +1,5 @@
-import { Remote, wrap } from "comlink";
-import { IMyClass } from "./defs";
+import { Remote, wrap } from 'comlink';
+import { IMyClass } from './defs';
 
 interface IRemoteMyClass extends Remote<IMyClass> {}
 
@@ -7,15 +7,17 @@ export class Local {
   constructor() {}
 
   async init(): Promise<void> {
-    this._worker = new Worker(new URL("./worker.js", import.meta.url), { type: 'module'});
-    console.log("worker", this._worker);
+    this._worker = new Worker(new URL('./worker.js', import.meta.url), {
+      type: 'module'
+    });
+    console.log('worker', this._worker);
 
     this._remoteObj = wrap(this._worker);
-    console.log("remoteObj", this._remoteObj);
+    console.log('remoteObj', this._remoteObj);
 
-    console.log("counter", await this._remoteObj.counter);
+    console.log('counter', await this._remoteObj.counter);
     this._remoteObj.increment();
-    console.log("counter", await this._remoteObj.counter);
+    console.log('counter', await this._remoteObj.counter);
   }
 
   private _worker?: Worker;
