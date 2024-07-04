b66c7b6752e845ace73a561f7cfd7ccfa84fb7c8
Ian Thomas <ianthomas23@gmail.com>
2024-07-04 14:04:30 +0100

    Use cockle 0.0.4 that contains WASM commands

--- src/worker.ts
+++ src/worker.ts
@@ -1,13 +1,14 @@
-import { expose } from 'comlink';
+import { Shell } from '@jupyterlite/cockle';
+import { DriveFS } from '@jupyterlite/contents';
 
-import { Shell } from './shell';
+import { expose } from 'comlink';
 
 import { IWorkerTerminal } from './tokens';
 
 class WorkerTerminal implements IWorkerTerminal {
   async initialize(options: IWorkerTerminal.IOptions): Promise<void> {
     this._options = options;
-    console.log('WorkerTerminal.initialize', this._options);
+    console.log('WorkerTerminal.initialize', this._options, this._wantDriveFS);
   }
 
   async input(text: string): Promise<void> {
@@ -19,8 +20,28 @@ class WorkerTerminal implements IWorkerTerminal {
   }
 
   async start(): Promise<void> {
-    this._shell = new Shell(this.output, this._options!.baseUrl);
-    this._shell.start();
+    this._shell = new Shell(this.output, this._mountpoint);
+    const { FS, PATH, ERRNO_CODES } = await this._shell.initFilesystem();
+
+    if (this._wantDriveFS) {
+      this._driveFS = new DriveFS({
+        FS,
+        PATH,
+        ERRNO_CODES,
+        baseUrl: this._options!.baseUrl,
+        driveName: '',
+        mountpoint: this._mountpoint
+      });
+      FS.mount(this._driveFS, {}, this._mountpoint);
+      FS.chdir(this._mountpoint);
+    } else {
+      // Add some dummy files if not using DriveFS.
+      FS.writeFile('file.txt', 'This is the contents of the file');
+      FS.writeFile('other.txt', 'Some other file');
+      FS.mkdir('dir');
+    }
+
+    await this._shell.start();
   }
 
   private async output(text: string): Promise<void> {
@@ -32,6 +53,9 @@ class WorkerTerminal implements IWorkerTerminal {
 
   private _options: IWorkerTerminal.IOptions | null = null;
   private _shell?: Shell;
+  private _mountpoint: string = '/drive';
+  private _wantDriveFS: boolean = true;
+  private _driveFS?: DriveFS;
 }
 
 const obj = new WorkerTerminal();
