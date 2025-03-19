94944d732293e8d8a769c1131bc928c6555b5988
Ian Thomas <ianthomas23@gmail.com>
2025-03-19 15:34:42 +0000

    Rewrite as JupyterLab frontend plugin

--- src/index.ts
+++ src/index.ts
@@ -2,84 +2,27 @@
 // Distributed under the terms of the Modified BSD License.
 
 import {
-  JupyterLiteServer,
-  JupyterLiteServerPlugin,
-  Router
-} from '@jupyterlite/server';
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+import { ITerminalManager, Terminal } from '@jupyterlab/services';
 
-import { TerminalManager } from './manager';
-import { ITerminalManager } from './tokens';
+import { LiteTerminalManager } from './manager';
 
 /**
- * The terminals service plugin.
+ * The terminal manager plugin.
  */
-const terminalsPlugin: JupyterLiteServerPlugin<ITerminalManager> = {
+const terminalManagerPlugin: JupyterFrontEndPlugin<Terminal.IManager> = {
   id: '@jupyterlite/terminal:plugin',
   description: 'A terminal for JupyterLite',
   autoStart: true,
   provides: ITerminalManager,
-  activate: async (app: JupyterLiteServer) => {
+  activate: async (app: JupyterFrontEnd): Promise<Terminal.IManager> => {
     console.log(
       'JupyterLite extension @jupyterlite/terminal:plugin is activated!'
     );
-
-    const { serviceManager } = app;
-    const { serverSettings, terminals } = serviceManager;
-    console.log('terminals available:', terminals.isAvailable());
-    console.log('terminals ready:', terminals.isReady); // Not ready
-    console.log('terminals active:', terminals.isActive);
-
-    // Not sure this is necessary?
-    await terminals.ready;
-    console.log('terminals ready after await:', terminals.isReady); // Ready
-
-    return new TerminalManager(serverSettings.wsUrl);
+    return new LiteTerminalManager();
   }
 };
 
-/**
- * A plugin providing the routes for the terminals service
- */
-const terminalsRoutesPlugin: JupyterLiteServerPlugin<void> = {
-  id: '@jupyterlite/terminal:routes-plugin',
-  autoStart: true,
-  requires: [ITerminalManager],
-  activate: (app: JupyterLiteServer, terminalManager: ITerminalManager) => {
-    console.log(
-      'JupyterLite extension @jupyterlite/terminal:routes-plugin is activated!',
-      terminalManager
-    );
-
-    // GET /api/terminals - List the running terminals
-    app.router.get('/api/terminals', async (req: Router.IRequest) => {
-      const res = await terminalManager.listRunning();
-      // Should return last_activity for each too,
-      return new Response(JSON.stringify(res));
-    });
-
-    // POST /api/terminals - Start a terminal
-    app.router.post('/api/terminals', async (req: Router.IRequest) => {
-      const res = await terminalManager.startNew();
-      // Should return last_activity too.
-      return new Response(JSON.stringify(res));
-    });
-
-    // DELETE /api/terminals/{terminal name} - Delete a terminal
-    app.router.delete(
-      '/api/terminals/(.+)',
-      async (req: Router.IRequest, name: string) => {
-        const exists = terminalManager.has(name);
-        if (exists) {
-          await terminalManager.shutdownTerminal(name);
-        } else {
-          const msg = `The terminal session "${name}"" does not exist`;
-          console.warn(msg);
-        }
-
-        return new Response(null, { status: exists ? 204 : 404 });
-      }
-    );
-  }
-};
-
-export default [terminalsPlugin, terminalsRoutesPlugin];
+export default [terminalManagerPlugin];
