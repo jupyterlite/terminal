9cf3651ea0a101e4c44744fd982a740ae9744a0e
Ian Thomas <ianthomas23@gmail.com>
2026-07-28 12:35:04 +0100

    Add more git2cpp tests

--- ui-tests/tests/command.spec.ts
+++ ui-tests/tests/command.spec.ts
@@ -68,30 +68,131 @@ test.describe('individual command', () => {
     });
 
     test(`should run git init`, async ({ page }) => {
-      await runCommand(page, `git init .`);
-      await runCommand(page, `ls .git > git1.txt 2> err1.txt`);
+      await runCommand(page, 'git init .');
+      await runCommand(page, 'tree -afi .git > git1.txt 2> err1.txt');
 
       let output = await retrieveAndDeleteFile(page, 'git1.txt');
       expect(output).toEqual([
-        'HEAD',
-        'config',
-        'description',
-        'hooks',
-        'info',
-        'objects',
-        'refs',
+        '.git',
+        '.git/HEAD',
+        '.git/config',
+        '.git/description',
+        '.git/hooks',
+        '.git/hooks/README.sample',
+        '.git/info',
+        '.git/info/exclude',
+        '.git/objects',
+        '.git/objects/info',
+        '.git/objects/pack',
+        '.git/refs',
+        '.git/refs/heads',
+        '.git/refs/tags',
+        '',
+        '9 directories, 5 files',
         ''
       ]);
       output = await retrieveAndDeleteFile(page, 'err1.txt');
       expect(output).toEqual(['']);
 
       await runCommand(page, `git status > git2.txt 2> err2.txt`);
-      await page.waitForTimeout(LONG_WAIT_MS);
-
       output = await retrieveAndDeleteFile(page, 'git2.txt');
       expect(output.slice(0, 2)).toEqual(['On branch master', 'No commit yet']);
       output = await retrieveAndDeleteFile(page, 'err2.txt');
       expect(output).toEqual(['']);
+
+      await runCommand(page, 'rm -rf .git');
+      await runCommand(page, 'ls .git 2> err3.txt');
+
+      output = await retrieveAndDeleteFile(page, 'err3.txt');
+      expect(output).toEqual(["ls: cannot access '.git': No such file or directory", ""]);
+    });
+
+    test('should run git init --bare', async ({ page }) => {
+      // Run twice to check PROXYFS.rename https://github.com/emscripten-forge/recipes/pull/6067
+      for (let repeat = 0; repeat < 2; repeat++) {
+        await runCommand(page, 'git init . --bare');
+        await runCommand(page, 'tree -afi > git1.txt 2> err1.txt');
+
+        let output = await retrieveAndDeleteFile(page, 'git1.txt');
+        expect(output).toEqual([
+          '.',
+          './HEAD',
+          './config',
+          './description',
+          './fact.lua',
+          './hooks',
+          './hooks/README.sample',
+          './info',
+          './info/exclude',
+          './months.txt',
+          './objects',
+          './objects/info',
+          './objects/pack',
+          './refs',
+          './refs/heads',
+          './refs/tags',
+          '',
+          '9 directories, 7 files',
+          ''
+        ]);
+        output = await retrieveAndDeleteFile(page, 'err1.txt');
+        expect(output).toEqual(['']);
+
+        await runCommand(page, 'rm -rf *');
+        await runCommand(page, 'ls * > git2.txt 2> err2.txt');
+
+        output = await retrieveAndDeleteFile(page, 'git2.txt');
+        expect(output).toEqual(['fact.lua', 'months.txt', '']);
+
+        output = await retrieveAndDeleteFile(page, 'err2.txt');
+        expect(output).toEqual(['']);
+
+        await runCommand(page, 'rm -rf *');
+      }
+    });
+
+    test('should clone remote repository', async ({ page }) => {
+      // Note: uses CORS proxy to access github repo.
+      // Run twice to check PROXYFS.rename https://github.com/emscripten-forge/recipes/pull/6067
+      for (let repeat = 0; repeat < 2; repeat++) {
+        await runCommand(page, 'git clone https://github.com/ianthomas23/cockle-playground');
+        await runCommand(page, 'tree -afi cockle-playground > git1.txt 2> err1.txt');
+
+        let output = await retrieveAndDeleteFile(page , 'git1.txt');
+        // Do not match all of the files as some contain hashes.
+        expect(output).toContainEqual('cockle-playground/LICENSE');
+        expect(output).toContainEqual('cockle-playground/README.md');
+        expect(output).toContainEqual('cockle-playground/package.json');
+        expect(output).toContainEqual('cockle-playground/.git');
+        expect(output).toContainEqual('cockle-playground/.git/FETCH_HEAD');
+        expect(output).toContainEqual('cockle-playground/.git/HEAD');
+        expect(output).toContainEqual('cockle-playground/.git/config');
+        expect(output).toContainEqual('cockle-playground/.git/description');
+        expect(output).toContainEqual('cockle-playground/.git/hooks');
+        expect(output).toContainEqual('cockle-playground/.git/hooks/README.sample');
+        expect(output).toContainEqual('cockle-playground/.git/index');
+        expect(output).toContainEqual('cockle-playground/.git/info');
+        expect(output).toContainEqual('cockle-playground/.git/info/exclude');
+        expect(output).toContainEqual('cockle-playground/.git/logs');
+        expect(output).toContainEqual('cockle-playground/.git/logs/HEAD');
+        expect(output).toContainEqual('cockle-playground/.git/logs/refs');
+        expect(output).toContainEqual('cockle-playground/.git/logs/refs/heads');
+        expect(output).toContainEqual('cockle-playground/.git/logs/refs/heads/main');
+        expect(output).toContainEqual('cockle-playground/.git/logs/refs/remotes');
+        expect(output).toContainEqual('cockle-playground/.git/objects');
+        expect(output).toContainEqual('cockle-playground/.git/objects/info');
+        expect(output).toContainEqual('cockle-playground/.git/objects/pack');
+        expect(output).toContainEqual('cockle-playground/.git/refs');
+        expect(output).toContainEqual('cockle-playground/.git/refs/heads');
+        expect(output).toContainEqual('cockle-playground/.git/refs/heads/main');
+        expect(output).toContainEqual('cockle-playground/.git/refs/remotes');
+        expect(output).toContainEqual('cockle-playground/.git/refs/tags');
+
+        output = await retrieveAndDeleteFile(page, 'err1.txt');
+        expect(output).toEqual(['']);
+
+        await runCommand(page, 'rm -rf cockle-playground *.txt');
+      }
     });
   });
 
