f5bab6ea790fc22321045300e10c2939d3be0c0f
Ian Thomas <ianthomas23@gmail.com>
2026-08-05 08:45:04 +0100

    Use correct extension directory name (#129)

--- jupyterlite_terminal/__init__.py
+++ jupyterlite_terminal/__init__.py
@@ -9,8 +9,11 @@ except ImportError:
     __version__ = "dev"
 
 
+__all__ = ["__version__", "_jupyter_labextension_paths"]
+
+
 def _jupyter_labextension_paths():
     return [{
         "src": "labextension",
-        "dest": "jupyterlite-terminal"
+        "dest": "@jupyterlite/terminal"
     }]
